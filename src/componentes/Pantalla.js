bc8d30e321b99d4c2fb5a61c8f976220d95121d5
Torben23 <105077862+Torben23@users.noreply.github.com>
2023-03-31 02:20:30 -0300

    Add files via upload

---
+++ src/componentes/Pantalla.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../hojas-de-estilo/Pantalla.css'
+
+const Pantalla = ({ input }) => (
+  <div className='input'>
+    {input}
+  </div>
+);
+
+export default Pantalla;
+
+
+
+
+
